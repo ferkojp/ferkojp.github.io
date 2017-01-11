180a091c43076538757ca5389126330bd67d4fde
Fernando Jp <ferko300@gmail.com>
2017-01-10 22:51:14 -0600

    add new site

--- js/js.js
+++ js/js.js
@@ -1,18 +1,7 @@
 $(document).ready(function(){
 
-	$("#contenido-menu-item-inn").click(function(){
-		$("#innovacion").toggle();
-		$("#auxiliar").hide();
-		$("#genera").hide();
+	$( window ).scroll(function() {
+  		
 	});
-	$("#contenido-menu-item-aux").click(function(){
-		$("#auxiliar").toggle();
-		$("#innovacion").hide();
-		$("#genera").hide();
-	});
-	$("#contenido-menu-item-gen").click(function(){
-		$("#genera").toggle();
-		$("#auxiliar").hide();
-		$("#innovacion").hide();
-	});
+
 });
