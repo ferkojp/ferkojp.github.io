20b3671e4e19eafec4afcf1a52fe4a9799f7f83f
Fernando Jp <ferko300@gmail.com>
2017-01-11 19:30:20 -0600

    delete trash

--- js/js.js
+++ js/js.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
 
-	$( window ).scroll(function() {
-  		
+
+	//Links sociales
+	$("#link_face").click(function(){
+		url = "https://www.facebook.com/ferko.jp";
+		window.open(url, '_blank');
+
+	});
+
+	$("#link_twitter").click(function(){
+		url = "https://twitter.com/ferko_jp";
+		window.open(url, '_blank');
+
+	});
+
+	$("#link_goo").click(function(){
+		url = "https://plus.google.com/u/0/102713484055196674457";
+		window.open(url, '_blank');
+
+	});
+
+	$("#link_link").click(function(){
+		url = "https://mx.linkedin.com/in/fernando-joaquin-pereyda-867a00107";
+		window.open(url, '_blank');
+
+	});
+
+	//Links portfolio
+	$("#link_innova").click(function(){
+		url = "http://innovaciontlaltenango.com.mx/";
+		window.open(url, '_blank');
+
+	});
+	$("#link_genera").click(function(){
+		url = "https://ferkojp.github.io/generaCV/";
+		window.open(url, '_blank');
+
 	});
 
 });
