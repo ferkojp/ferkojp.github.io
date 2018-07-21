469a2f6748f282b345af7fad0c07451d505b7d24
fkojp <ferko300@gmail.com>
2018-07-21 10:38:38 -0500

    Eliminar archivos viejos

--- generaCV/js/mlds.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-		var doc = new jsPDF();
-		var html=$("#basico").html();
-		doc.fromHTML(html);
-		var string = doc.output('datauristring');
-		$('#iframe_content').attr('src', string);
-
- });
-
--- generaCV/readme.txt
+++
@@ -1,2 +0,0 @@
-GeneraCV es un producto creado y elaborado por
-FkoSystems 
